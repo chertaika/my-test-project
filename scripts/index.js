87e863dff0606bd0fad10bc768918015ba11e683
Ekaterina Iurkova <chertaika1439@yandex.ru>
2023-01-16 16:38:01 +0700

    добавлен sound

---
+++ scripts/index.js
@@ -0,0 +1,21 @@
+let audio = document.querySelector('.lead__audio');
+let audioButton = document.querySelector('.lead__audio-btn');
+
+audioButton.addEventListener('click', () => {
+  let pause = audioButton.classList.contains('lead__audio-btn_pause');
+  if(pause) {
+    audioButton.classList.remove('lead__audio-btn_pause');
+  } else {
+    audioButton.classList.add('lead__audio-btn_pause');
+  }
+
+  let method = pause ? 'pause' : 'play';
+  audio[method]();
+})
+
+document.addEventListener("visibilitychange", function() {
+  if (document.hidden){
+    audio['pause']();
+    audioButton.classList.remove('lead__audio-btn_pause');
+}
+});
